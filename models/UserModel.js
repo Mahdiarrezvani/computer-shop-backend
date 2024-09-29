93716258b91db681b3c8a92177ddf48fba4992d0
Mahdiar <mahdiarrezvani59@gmail.com>
2024-09-29 10:59:56 +0330

    add User Model

---
+++ models/UserModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose.schema({
+  first_name: {
+    required: true,
+    type: String,
+  },
+  last_name: {
+    required: true,
+    type: String,
+  },
+  email: {
+    required: true,
+    type: String,
+  },
+  phone_number: {
+    required: true,
+    type: String,
+  },
+  img: {
+    required: true,
+    type: String,
+  },
+});
+
+const UserModel = mongoose.models.User || mongoose.model("User", schema);
+
+export default UserModel;
