032bddfd6074f9cbd6c91d4a6947119965f58b40
Mahdiar <mahdiarrezvani59@gmail.com>
2024-09-30 14:38:26 +0330

    add api route product

---
+++ app/api/products/[id]/route.js
@@ -0,0 +1,68 @@
+import connectToDB from "@/utils/db";
+
+export async function DELETE(req, { params }) {
+  connectToDB();
+
+  if (!isValidObjectId(params.id)) {
+    return new Response(
+      JSON.stringify({
+        message: "ایدی معتبر نمیباشد",
+      }),
+      {
+        status: 404,
+      }
+    );
+  }
+
+  const isUser = await ProductModel.findOne({ _id: params.id });
+  if (!isUser) {
+    return new Response(
+      JSON.stringify({
+        message: "ایدی محصول اشتباه است",
+      }),
+      {
+        status: 422,
+      }
+    );
+  }
+
+  const products = await ProductModel.findOneAndDelete({ _id: params.id });
+  return Response.json({ message: "محصول با موفقیت حذف شد", data: products });
+}
+
+export async function PUT(req, { params }) {
+  connectToDB();
+  const body = req.json();
+
+  if (!isValidObjectId(params.id)) {
+    return new Response(
+      JSON.stringify({
+        message: "ایدی معتبر نمیباشد",
+      }),
+      {
+        status: 404,
+      }
+    );
+  }
+
+  const isUser = await ProductModel.findOne({ _id: params.id });
+  if (!isUser) {
+    return new Response(
+      JSON.stringify({
+        message: "ایدی محصول اشتباه است",
+      }),
+      {
+        status: 422,
+      }
+    );
+  }
+
+  const products = await ProductModel.findOneAndUpdate(
+    { _id: params.id },
+    { ...body }
+  );
+  return Response.json({
+    message: "محصول با موفقیت ویرایش شد",
+    data: products,
+  });
+}
---
+++ app/api/products/route.js
@@ -0,0 +1,15 @@
+import ProductModel from "@/models/ProductModel";
+import connectToDB from "@/utils/db";
+
+export async function POST(req) {
+  connectToDB();
+  const body = req.json();
+  const products = await ProductModel.create({ ...body });
+  return Response.json({ message: "محصول با موفقیت انجام شد", data: products });
+}
+
+export async function GET() {
+  connectToDB();
+  const products = await ProductModel.find({});
+  return Response.json({ message: "لیست محصولات", data: products });
+}
